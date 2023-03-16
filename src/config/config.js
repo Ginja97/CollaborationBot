88a5771e3e052f6039f2a8aca2c43222f2703f98
Ginja97 <nicolas.pusch1997@gmail.com>
2023-03-16 14:10:50 +0000

    added cookies to .env

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,10 @@ const config = {
     env: process.env.NODE_ENV || "development",
     port: process.env.PORT || 3001,
     redditUsername: procedd.env.REDDIT_USERNAME,
-    redditPassword: process.env.REDDIT_PASSWORD
+    redditPassword: process.env.REDDIT_PASSWORD,
+    honeygain_cookies: process.env.HONEYGAIN_COOKIES,
+    genshin_cookies_me: process.env.GENSHIN_COOKIES_ME,
+    genshin_cookies_sasha: process.env.GENSHIN_COOKIES_SASHA
 };
 
 module.exports = config;
