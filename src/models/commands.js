24a248e51995ac442ae0e3631d5178000abcb442
Ginja97 <nicolas.pusch1997@gmail.com>
2023-03-21 12:14:05 +0000

    indentation

--- src/models/commands.js
+++ src/models/commands.js
@@ -8,11 +8,13 @@ const honeygain_command = ""
 const genshin_command_me = ""
 const genshin_command_sasha = ""
 
+
 command_aliases = {
     honeygain_command: "HGC",
     genshin_command_me: "GCM",
     genshin_command_sasha: "GCS",
 }
+
 
 commands_list = {
     "HGC": honeygain_command,
