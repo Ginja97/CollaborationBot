5b0a95698b923ee8825444ebeb9fcd07d0cf2a2a
Ginja97 <nicolas.pusch1997@gmail.com>
2023-03-21 12:21:31 +0000

    added database connection

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
 const config = {
-    env: process.env.NODE_ENV || "development",
     port: process.env.PORT || 3001,
     redditUsername: procedd.env.REDDIT_USERNAME,
     redditPassword: process.env.REDDIT_PASSWORD,
