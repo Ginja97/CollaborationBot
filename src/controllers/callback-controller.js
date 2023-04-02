d2f9d89b4a27b186a4600645ffbbaa568e02931a
Ginja97 <nicolas.pusch1997@gmail.com>
2023-04-02 12:51:31 +0000

    background controller implemented, not yet tested

--- src/controllers/callback-controller.js
+++ src/controllers/callback-controller.js
@@ -1,6 +1,6 @@
 const config = require('../config/config')
 const log = require('../utils/logger')
-const fetch = require('node-fetch')
+import fetch from 'node-fetch';
 
 function craftGenshinHeaders(extraHeaders) {
     let headers = config.genshinBrowserHeaders
@@ -12,10 +12,19 @@ function craftGenshinHeaders(extraHeaders) {
 
 
 function sendRequest(method, url, headers, body) {
-    fetch(url, {
-        method: method,
-        body: JSON.stringify(body)
-    })
+    // return promise of response data
+    return fetch(url, {
+                        method: method,
+                        body: JSON.stringify(body),
+                        headers: headers
+                })
+            .then((response) => {
+                log.logString('[CALLBACK][EXECUTION][INFO]', `callback ${method} @ ${url} responded with: ${JSON.stringify(response)}`)
+                return response.json()
+            })
+            .catch((error) => {
+                log.logString('[CALLBACK][EXECUTION][ERROR]', error)
+            })
 }
 
 
@@ -28,8 +37,8 @@ function executeCallback(method, url, headers, body, callback_id) {
         break;
     case 2:
         // genshin daily login
-        let headers = craftGenshinHeaders(headers)
-
+        let full_headers = craftGenshinHeaders(headers)
+        response = sendRequest(method, url, full_headers, body)
         break;
     default:
         log.logString('[CALLBACK][WARNING]', `No callback found for callback_id: ${callback_id} with request: ${method} @ ${url}`)
