8c0f1c4bc2bc4ff89af2251a3680926dbcc06da0
Ginja97 <nicolas.pusch1997@gmail.com>
2023-04-02 11:56:21 +0000

    added normal browser headers to config

--- src/config/config.js
+++ src/config/config.js
@@ -8,6 +8,22 @@ const config = {
     honeygain_cookies: process.env.HONEYGAIN_COOKIES,
     genshinCookiesMe: process.env.GENSHIN_COOKIES_ME,
     genshinCookiesSasha: process.env.GENSHIN_COOKIES_SASHA,
+    genshinBrowserHeaders: {
+        "Host": "sg-hk4e-api.hoyolab.com", 
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:108.0) Gecko/20100101 Firefox/108.0", 
+        "Accept": "application/json, text/plain, */*", 
+        "Accept-Language": "en-US,en;q=0.5", 
+        "Accept-Encoding": "gzip, deflate, br", 
+        "Content-Type": "application/json;charset=utf-8", 
+        "Content-Length": "29", 
+        "Origin": "https://act.hoyolab.com", 
+        "Connection": "keep-alive", 
+        "Referer": "https://act.hoyolab.com/", 
+        "Sec-Fetch-Dest": "empty", 
+        "Sec-Fetch-Mode": "cors", 
+        "Sec-Fetch-Site": "same-site", 
+        "TE": "trailers",
+    },
     // pg-promise options
     collaborationDbConfig: {
         database: process.env.DB_DATABASE,
